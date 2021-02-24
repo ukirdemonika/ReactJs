b30b92843c1456e42a0f5c2b80891e61a1b71de4
Monika Ukirde <ukirdemonika19@gmail.com>
2021-02-24 16:49:10 +0530

    States

--- react-bootcamp/portfolio/src/App.js
+++ react-bootcamp/portfolio/src/App.js
@@ -1,19 +1,37 @@
 import React,{Component}  from 'react';
 
-class Regularclass{}
-class Componentclass extends Component{}
-const regularclass=new Regularclass();
-const componentclass=new Componentclass();
-console.log("Regular class=",regularclass);
-console.log("componentclass=",componentclass);
+// class Regularclass{}
+// class Componentclass extends Component{}
+// const regularclass=new Regularclass();
+// const componentclass=new Componentclass();
+// console.log("Regular class=",regularclass);
+// console.log("componentclass=",componentclass);
 
 class App extends Component{
+    constructor(){
+        super();
+        this.state={displayBio:false};   //change false to true then visible the state div on webpage
+    }
     render(){
+        // const bio=this.state.displayBio?(
+        //     <div>
+        //             <p>I live in India.</p>
+        //             <p>I like javascript, and ReactJs.</p>
+        //     </div>
+        // ):null;       
         return(
             <div>
                 <h1>Hello,</h1>
                 <p>My name is Monika.I'm a software engineer.</p>
-
+                {/* {bio} */}
+                {
+                    this.state.displayBio?(
+                        <div>
+                            <p>I live in India.</p>
+                            <p>I like javascript, and ReactJs.</p>
+                        </div>
+                    ):null  
+                } 
             </div>
         )
     }
--- react-bootcamp/portfolio/src/index.js
+++ react-bootcamp/portfolio/src/index.js
@@ -4,34 +4,34 @@ import App from './App';
 // ReactDOM.render(<div>React app</div>,document.getElementById("root"));
 ReactDOM.render(<App />,document.getElementById("root"));
 
-//classes ,inheritance and closer compoentnt look
-class Animal{
-    constructor(name,age){
-        this.name=name;
-        this.age=age
-    }
-    speak(){
-        console.log("I am",this.name,"I am",this.age,"years old")
-    }
+// //classes ,inheritance and closer compoentnt look
+// class Animal{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age
+//     }
+//     speak(){
+//         console.log("I am",this.name,"I am",this.age,"years old")
+//     }
     
-}
-const animal1=new Animal("Lion",40);
-animal1.speak();
-console.log(animal1);
+// }
+// const animal1=new Animal("Lion",40);
+// animal1.speak();
+// console.log(animal1);
 
 
-//inheritance
-class Lion extends Animal{
-    constructor(name,age,furColor,speed){
-        super(name,age);
-        this.furColor=furColor;
-        this.speed=speed;
-    }
-    roar(){
-        console.log("Rooooor I have ",this.furColor,"fur,and I can run",this.speed,"miler per hour");
-    }
-}
-const lion=new Lion("Mufasa",20,"golder",40);
-lion.speak();
-lion.roar();
-console.log(lion);
+// //inheritance
+// class Lion extends Animal{
+//     constructor(name,age,furColor,speed){
+//         super(name,age);
+//         this.furColor=furColor;
+//         this.speed=speed;
+//     }
+//     roar(){
+//         console.log("Rooooor I have ",this.furColor,"fur,and I can run",this.speed,"miler per hour");
+//     }
+// }
+// const lion=new Lion("Mufasa",20,"golder",40);
+// lion.speak();
+// lion.roar();
+// console.log(lion);
