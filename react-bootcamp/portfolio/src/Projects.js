9534a0aee2003509083818d2a79cd7a2e5ff9ad7
Monika Ukirde <ukirdemonika19@gmail.com>
2021-03-01 15:12:03 +0530

    Props

--- react-bootcamp/portfolio/src/Projects.js
+++ react-bootcamp/portfolio/src/Projects.js
@@ -1,6 +1,24 @@
 import React,{Component}from 'react';
 import PROJECTS from './data/projects';
 
+//props
+class Project extends Component{
+   
+    render(){
+        console.log(this.props);
+        const { title,image,description,link}=this.props.Project;
+        return(
+            //<div>{this.props.Project.title}</div>
+            <div style={{display:'inline-block',width:300,margin:10}}>
+                <h3>{title}</h3>
+                <img src={image} alt="profile" style={{ width:200, height:120}}/>
+                <p>{description}</p>
+                <a href={link}>{link}</a>
+            </div>
+        );
+    }
+}
+
 class Projects extends Component{
     render(){
         return(
@@ -9,9 +27,11 @@ class Projects extends Component{
             <div>
                 {
                     //to transform the project data items to individual pieces of JSX to render.
-                    PROJECTS.map(PROJECTS=>{     
+                    PROJECTS.map(PROJECT=>{     
                         return (
-                            <div key={PROJECTS.id}>{PROJECTS.title}</div>
+                           // <div key={PROJECT.id}>{PROJECT.title}</div>
+                           //using props
+                           <Project  key={PROJECT.id} Project={PROJECT}/>
                         )
                     })
                 }
